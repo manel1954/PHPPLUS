c451afe4a55c8a6b391222dc4df1fe4bfc24820a
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-15 19:22:57 +0200

    Update MMDVM_BUENO_ENCURSO.php

--- html/MMDVM_BUENO_ENCURSO.php
+++ html/MMDVM_BUENO_ENCURSO.php
@@ -319,7 +319,7 @@ if ($action === 'ysf-transmission') {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Panel ADER</title>
+<title>PANEL PHP2</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=Rajdhani:wght@500;700&family=Orbitron:wght@700;900&display=swap" rel="stylesheet">
 <style>
