2af3c8be4258e4666659437897a274f26b625e34
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-19 22:17:28 +0200

    Update mmdvmcopia.php

--- html/mmdvmcopia.php
+++ html/mmdvmcopia.php
@@ -545,7 +545,7 @@ button.btn-header { font-family: var(--font-mono); }
 
 .nx-infobar { position: absolute; top: 30px; left: 0; right: 0; height: 26px; background: rgba(0,0,0,.35); border-bottom: 1px solid #0d2030; display: flex; align-items: center; justify-content: space-around; padding: 0 3rem; gap: 1rem; z-index: 2; }
 .nx-info-item { display: flex; align-items: center; gap: .4rem; }
-.nx-info-lbl { font-family: var(--font-mono); font-size: 7px; color:#ff0; letter-spacing: .12em; text-transform: uppercase; }
+.nx-info-lbl { font-family: var(--font-mono); font-size: 7px; color:#ff0; letter-spacing: 2px; text-transform: uppercase; }
 .nx-info-val { font-family: var(--font-mono); font-size: 7px; color: var(--text); letter-spacing: .06em; font-weight: bold; }
 
 
