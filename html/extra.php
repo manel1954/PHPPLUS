6102eb0b7a77b42757df4fe5042e5a795a78c52b
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-20 21:51:07 +0200

    Update extra.php

--- html/extra.php
+++ html/extra.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title style="color:#fff;">MENU EXTRA.</title>
+    <title>MENU EXTRA</title>
     <style>
     body { background-color: #120f0f;
     }
@@ -12,7 +12,7 @@
     </style>
 </head>
 <body>
-    <h1><?php echo "MENU EXTRA"; ?></h1>
+    <h1 style="color: white;"><?php echo "MENU EXTRA"; ?></h1>
     
     <button class="btn-header" onclick="extraOpen()">⌨ DUMP1090</button>
     
