57f37a6b83727efc10a5caa6e3cb99886d3cc455
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-20 22:25:22 +0200

    Update extra.php

--- html/extra.php
+++ html/extra.php
@@ -6,18 +6,62 @@
     <title>MENU EXTRA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+    <style>
+        .navbar-granate {
+            background-color: #6b0f1a;
+            min-height: 30px;
+            padding-top: 0;
+            padding-bottom: 0;
+        }
+        .navbar-granate .nav-link {
+            color: #fff;
+            font-size: 0.85rem;
+            padding-top: 5px;
+            padding-bottom: 5px;
+        }
+        .navbar-granate .nav-link:hover {
+            color: #ffcccc;
+        }
+        .navbar-granate .navbar-brand {
+            padding-top: 0;
+            padding-bottom: 0;
+        }
+        .navbar-granate .navbar-brand img {
+            height: 24px;
+        }
+    </style>
 </head>
 <body class="bg-dark text-white">
 
+<!-- HEADER -->
+<nav class="navbar navbar-expand-md navbar-granate">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="icon.png" alt="Logo">
+        </a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+            <span class="navbar-toggler-icon" style="filter: invert(1);"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="mainNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Enlace 2</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Enlace 3</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Enlace 4</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- CONTENIDO -->
 <div class="container py-4">
 
-<h1 class="mb-4 text-center">
-    <i class="bi bi-grid-3x3-gap-fill me-2" style="color: #ff6600;"></i>
-    <?php echo "MENU EXTRA"; ?>
-</h1>
+    <h1 class="mb-4 text-center">
+        <i class="bi bi-grid-3x3-gap-fill me-2" style="color: #ff6600;"></i>
+        <?php echo "MENU EXTRA"; ?>
+    </h1>
 
     <div class="row g-3">
-
         <div class="col-12 col-sm-6 col-md-4">
             <div class="card bg-secondary border-0 h-100">
                 <div class="card-body d-flex flex-column">
@@ -31,9 +75,6 @@
                 </div>
             </div>
         </div>
-
-        <!-- Añade más tarjetas aquí con el mismo bloque col -->
-
     </div>
 
 </div>
