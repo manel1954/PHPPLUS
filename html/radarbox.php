93385398e4c0ab0e1c44d52d79974d6f3ac4a5cc
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-10 11:38:38 +0200

    Update radarbox.php

--- html/radarbox.php
+++ html/radarbox.php
@@ -70,7 +70,8 @@ $config_content = file_exists($config_file) ? file_get_contents($config_file) : 
 <title>Radarbox</title>
 
 <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📡</text></svg>">
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
 <style>
 body {
     background:#0f172a;
