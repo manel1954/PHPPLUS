2aa50231293afabee2dcd695df4aef8ffdf5f27a
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-20 23:45:26 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -6,7 +6,7 @@
                         sudo rm -R /home/pi/A108
                         mkdir /home/pi/A108                                                
                         cp -R /home/pi/PHPPLUS/* /home/pi/A108
-                        cp -R /home/pi/PHPPLUS/html/ /var/www/
+                        cp -R /home/pi/A108/html/ /var/www/
                         sleep 6                                             
                         sudo chmod 777 -R /home/pi/A108   
                         sudo chmod 777 -R /var/www/html
