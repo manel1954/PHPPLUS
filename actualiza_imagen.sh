77f1a8e957bf07461d31311ecdd226b30416b19d
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-21 00:19:18 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
 git config --global --add safe.directory /home/pi/PHPPLUS
 cd /home/pi/PHPPLUS                                             
-git pull --force                      
+git pull --force
+
+# Guardar password.json ANTES de tocar nada
+cp /var/www/html/password.json /tmp/password_backup.json
+
 sudo rm -R /home/pi/A108
 mkdir /home/pi/A108                                                
 cp -R /home/pi/PHPPLUS/* /home/pi/A108
 sudo rm -R /home/pi/A108/html
-shopt -s extglob
-cp -R /home/pi/PHPPLUS/html/!(password.json) /var/www/html/
+cp -R /home/pi/PHPPLUS/html/ /var/www/
+
+# Restaurar password.json real (sobreescribe el del github)
+cp /tmp/password_backup.json /var/www/html/password.json
+
 sleep 6                                             
 sudo chmod 777 -R /home/pi/A108   
 sudo chmod 777 -R /var/www/html
