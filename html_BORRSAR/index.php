7a37cfe30fbf2ba9b409f03420934dab5535759a
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-23 01:14:41 +0200

    1

--- html_BORRSAR/index.php
+++
@@ -1,2 +0,0 @@
-<!-- echo '<?php header("Location: mmdvm.php"); exit; ?>' | sudo tee /var/www/html/index.php -->
-echo '<?php header("Location: login.php"); exit; ?>' | sudo tee /var/www/html/index.php
