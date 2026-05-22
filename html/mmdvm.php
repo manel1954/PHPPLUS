7b26197c7789ad46002cca667b436d71b6c0714a
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-22 20:09:31 +0200

    Update mmdvm.php

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -849,7 +849,7 @@ button.btn-header { font-family: var(--font-mono); }
   </div>
 </div>
 <button class="btn-header cyan" onclick="xtTtydOpen()">⌨ Terminal</button>
-<a href="extra.php" class="btn-header amber">☰ Menu Extra</a>
+<a href="extra.php" target="_blank" class="btn-header amber">☰ Menu Extra</a>
 <button id="btnReboot" class="btn-header red" onclick="rebootPi()">⏻ Reiniciar Pi</button>
 </div>
 </header>
