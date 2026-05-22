cf31a7ca97c2b78660a7bc56963db3af4d24a849
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-23 01:07:52 +0200

    Update mmdvm.php

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -908,7 +908,7 @@ button.btn-header { font-family: var(--font-mono); }
 <body>
 <header class="ctrl-header">
 <div class="ctrl-header-top">
-<a href="http://rem-esp.es" target="_blank">
+<a href="https://associacioader.com" target="_blank">
   <img src="Logo_Ader.png" alt="EA4RCR" style="height:40px;width:auto;">
 </a>
 <h1>SISTEMA DE CONTROL Y MONITORIZACIÓN PARA RADIOAFICIONADOS PHPPlus</h1>
