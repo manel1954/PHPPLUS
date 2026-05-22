a49cebf220346c1b08caec028a5f168c80ac507d
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-22 20:29:42 +0200

    Update mmdvm.php

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -832,9 +832,12 @@ button.btn-header { font-family: var(--font-mono); }
 <body>
 <header class="ctrl-header">
 <div class="ctrl-header-top">
+<!-- <img src="Logo_Ader.png" alt="EA3EIZ" style="height:40px;width:auto;"> -->
+
         <a class="navbar-brand" href="https://associacioader.com/" target="_blank">
-          <img src="Logo_Ader.png" alt="Logo">
+          <img src="Logo_Ader.png" alt="Logo"style="height:40px;width:auto;>
         </a>
+
 <h1>PANEL SISTEMAS DIGITALES PARA RADIOAFICIONADOS PHPPLUS</h1>
 </div>
 <div class="ctrl-header-btns">
