78a8fb306f3d5ac039da764fd91866cb0f361a83
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-22 21:47:52 +0200

    Update esp32.php

--- html/esp32.php
+++ html/esp32.php
@@ -73,12 +73,13 @@ button{ background:var(--primary); color:white; } button:hover{ opacity:.9; } bu
     <button type="button" style="background-color: #673AB7;">📥 Descarga firmware</button>
 </a>
 
-<a href="mmdvm.php" style="text-decoration:none; margin-left:auto;">
+<a href="#" onclick="window.close();style="text-decoration:none; margin-left:auto;">
     <button type="button">
-        🏠 Panel PHPPLUS
+        🏠 CERRAR ESP32
     </button>
 </a>
-    </div>
+    
+</div>
     <div id="progressContainer" class="progress-wrapper hidden">
         <div class="progress-bar"><div id="progressFill" class="progress-fill">0%</div></div>
         <div id="progressLabel" style="margin-top:8px; font-size:0.9rem; color:#bbb; text-align:center;">Esperando...</div>
