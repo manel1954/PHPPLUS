c6dab3aa229da3f195b917a52a866fa48533b835
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-22 23:14:41 +0200

    Update mmdvm.php

--- html/mmdvm.php
+++ html/mmdvm.php
@@ -832,7 +832,7 @@ button.btn-header { font-family: var(--font-mono); }
 <body>
 <header class="ctrl-header">
 <div class="ctrl-header-top">
-<img src="Logo_ea3eiz.png" alt="EA3EIZ" style="height:40px;width:auto;">
+<img src="Logo_Ader.png" alt="EA3EIZ" style="height:40px;width:auto;">
 <h1>PANEL SISTEMAS DIGITALES PARA RADIOAFICIONADOS PHPPLUS</h1>
 </div>
 <div class="ctrl-header-btns">
