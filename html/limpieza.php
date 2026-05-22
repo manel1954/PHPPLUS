ea5e4b5eab9d47279653d4cf748d163d5d21a9bc
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-22 22:01:59 +0200

    Update limpieza.php

--- html/limpieza.php
+++ html/limpieza.php
@@ -318,8 +318,10 @@ button:hover{
 
 <div class="top">
 <h1>🧹 Limpieza del sistema</h1>
-<a href="#" onclick="window.close(); return false;" class="btn btn-outline-light btn-sm">
-    <i class="bi bi-x-circle me-1"></i> CERRAR LIMPIEZA
+<a href="#" onclick="window.close();"style="text-decoration:none; margin-left:auto;">
+    <button type="button">
+        🏠 CERRAR LIMPIEZA
+    </button>
 </a>
 </div>
 
