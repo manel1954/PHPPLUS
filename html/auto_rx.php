a818a4d695c37b38f8fe40fd9caed4f3ad04f0e4
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-22 21:31:56 +0200

    Update auto_rx.php

--- html/auto_rx.php
+++ html/auto_rx.php
@@ -182,10 +182,8 @@ setInterval(() => {
 </button>
 
 
-<a href="mmdvm.php">
-    <button type="button">
-        <i class="bi bi-house-fill me-1"></i> Panel PHPPLUS
-    </button>
+<a href="#" onclick="window.close(); return false;" class="btn btn-outline-light btn-sm">
+    <i class="bi bi-x-circle me-1"></i> CERRAR AUTO_RX
 </a>
 
 
