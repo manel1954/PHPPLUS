ccb6ec78ef949a8ad3db1850f9ce45851f7691de
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-05-23 14:04:30 +0200

    Update fusion2x.php

--- html/fusion2x.php
+++ html/fusion2x.php
@@ -577,7 +577,7 @@ body{
                 Emisión / radio en tiempo real
             </div>
 
-            <div class="fusion-sub" style="color:var(--green); margin-top:4px;font-size:3rem;">
+            <div class="fusion-sub" style="color:var(--green); margin-top:4px;font-size:2rem;">
                 No disponible (Temporalmente en construcción)
             </div>
 
